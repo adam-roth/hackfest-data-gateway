1f01e8c06d43a10df836191c05e56146702885b3
Adam Roth <aroth.bigtribe@gmail.com>
2015-11-21 13:11:31 +1000

    fix issues with missing json dependency in WAR build

--- data-gateway/grails-app/conf/BuildConfig.groovy
+++ data-gateway/grails-app/conf/BuildConfig.groovy
@@ -32,15 +32,16 @@ grails.project.dependency.resolution = {
         mavenCentral()
 
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://snapshots.repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         runtime 'mysql:mysql-connector-java:5.1.22'
+		compile 'com.googlecode.json-simple:json-simple:1.1.1'
     }
 
     plugins {
